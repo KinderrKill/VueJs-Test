d38e0ed2717ce19f8a3706ce907f25b9e6ebfebc
KinderrKill <sauvage-etienne@orange.fr>
2022-01-14 14:48:00 +0100

    First commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="navbar">
+        <h1>Hello World.</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main-page">
+        <h1>Main Page</h1>
+        <Navbar />
+    </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+    name: "MainPage",
+    components: {
+        Navbar
+    }
+}
+</script>
